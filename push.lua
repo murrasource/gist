6c65bb74f64d9b5f9b2c385b5497d37ce5f15b39
Austin Murr <murrwebserver@protonmail.com>
2023-10-30 12:17:18 -0500

    Typo in lua

--- push.lua
+++ push.lua
@@ -43,7 +43,7 @@ function dovecot_lua_notify_event_message_new(context, event)
     local status = maildir:status(dovecot.storage.STATUS_RECENT, dovecot.storage.STATUS_UNSEEN, dovecot.storage.STATUS_MESSAGES)
     maildir:free()
     table.insert(context.messages, {
-        user = content.user.username,
+        user = context.user.username,
         to = event.to_address,
         uidvalidity = event.uid_validity,
         uid = event.uid,
