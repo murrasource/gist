1545cc4b42527beaad6d3cb01f469538d1dec50b
Austin Murr <murrwebserver@protonmail.com>
2023-11-02 12:47:44 -0500

    Make the directory

--- processor/mail_utils.py
+++ processor/mail_utils.py
@@ -156,6 +156,7 @@ class Maildir:
             if VirtualUser.objects.filter(email=f'{user}@gist.email'):
                 path = Path(f'{settings.GIST_REPORT_PREFIX}/{user}/{settings.MAILDIR_NAME}/')
                 path.parent.mkdir(parents=True, exist_ok=True)
+                path.mkdir()
             self.user: str = user
             self.root: str = get_maildir_path(user)
             self.path: str = self.root
