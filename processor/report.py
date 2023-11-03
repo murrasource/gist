3d836649e805b581d69f8dbed643254aa3160772
Austin Murr <murrwebserver@protonmail.com>
2023-11-03 14:50:30 -0500

    Final formatting updates

--- processor/report.py
+++ processor/report.py
@@ -14,7 +14,7 @@ def get_report_path(report: EmailGistReport):
     return f'{settings.GIST_REPORT_PREFIX}/{get_username_from_address(report.account.user.email)}/{settings.GIST_REPORT_FOLDER}/{report.uuid}'
 
 def write_report_email(report: EmailGistReport):
-    html = render_to_string('report.html', {'report': report, 'folders': [gist.category for gist in report.gists.all()]})
+    html = render_to_string('report.html', {'report': report, 'folders': list(set([gist.category for gist in report.gists.all()]))})
     with open(report.location, 'a+') as content:
         content.write(html)
         content.close()
